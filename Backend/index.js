a098808f542b6d3689f3ace5359823db49cc93b2
Deepak Kumar <deepakk8002958412@gmail.com>
2026-03-12 01:06:31 +0530

    Initial commit

---
+++ Backend/index.js
@@ -0,0 +1,51 @@
+
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
+
+dotenv.config();
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+const URI = process.env.MONGODB_URI;
+
+// MongoDB connection
+mongoose.connect(URI)
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((error) => {
+    console.log("MongoDB connection error:", error);
+});
+
+// routes
+app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
+
+// code for deploying the server
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+
+   app.use(express.static(path.join(__dirname, "Frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "Frontend/dist", "index.html"));
+});
+}
+
+// start server
+server.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
+});
+
